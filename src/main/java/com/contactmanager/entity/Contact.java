81a38b54dbea57cc34f5d65e9f59d3a21d8c6c5a
Hrushikesh Sawant <hrushikeshsawant22@gmail.com>
2024-03-10 22:36:17 +0530

    Created home and base template

--- src/main/java/com/contactmanager/entity/Contact.java
+++ src/main/java/com/contactmanager/entity/Contact.java
@@ -1,4 +1,4 @@
-package com.contactmanager.entities;
+package com.contactmanager.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/com/contactmanager/entity/User.java
+++ src/main/java/com/contactmanager/entity/User.java
@@ -1,4 +1,4 @@
-package com.contactmanager.entities;
+package com.contactmanager.entity;
 
 import java.util.ArrayList;
 import java.util.List;
