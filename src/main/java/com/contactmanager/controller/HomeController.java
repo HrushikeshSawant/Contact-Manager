dc18ef88c3d62236d0c686fcf2962a47c2f8c0ff
Hrushikesh Sawant <hrushikeshsawant22@gmail.com>
2024-03-26 21:04:33 +0530

    Created api and HTML page for sign up page

--- src/main/java/com/contactmanager/controller/HomeController.java
+++ src/main/java/com/contactmanager/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.contactmanager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,7 +11,8 @@ public class HomeController {
 	@GetMapping("/")
 	public String home(Model model)
 	{
-		model.addAttribute("title", "Home Page");
+		model.addAttribute("title", "Free Contact Manager | Contact Manager");
+		model.addAttribute("homeappend", "active");
 		return "home";
 	}
 	
@@ -19,6 +21,14 @@ public class HomeController {
 	{
 		model.addAttribute("title", "About Page");
 		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model)
+	{
+		model.addAttribute("title", "Sign Up | Contact Manager");
+		model.addAttribute("signupappend", "active");
+		return "signup";
 	}
 	
 }
