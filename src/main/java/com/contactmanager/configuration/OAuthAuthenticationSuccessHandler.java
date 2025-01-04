34e263aa1751e2fbc240372c086faaeb076dbdfe
Hrushikesh Sawant <hrushikeshsawant22@gmail.com>
2025-01-05 01:31:12 +0530

    Implemented OAuth2 authentication using github

--- src/main/java/com/contactmanager/configuration/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/contactmanager/configuration/OAuthAuthenticationSuccessHandler.java
@@ -1,14 +1,17 @@
 package com.contactmanager.configuration;
 
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -33,21 +36,57 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 		
+		String fetchedEmail = "";
+		
 		DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
-//		principal.getAttributes().forEach((key, value) -> log.info(key + " => " + value));
-//		log.info(principal.getName());
-//		principal.getAuthorities().forEach(System.out::println);
+		
+		OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+		String authorizedClientRegistrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+		log.info(authorizedClientRegistrationId);
 		
 		User user = new User();
-		user.setName(principal.getAttribute("name").toString());
-		user.setEmail(principal.getAttribute("email").toString());
-		user.setProfilePic(principal.getAttribute("picture").toString());
-		user.setEnabled(true);
-		user.setEmailVerified(principal.getAttribute("email_verified"));
-		user.setProvider(Providers.GOOGLE);
-		user.setProviderUserId(principal.getName());
-		user.setRoleList(List.of(ApplicationConstants.ROLE_USER));
 		
+		if(authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+			
+//			System.out.println("google");
+//			principal.getAttributes().forEach((key, value) -> log.info(key + " => " + value));
+//			log.info(principal.getName());
+//			principal.getAuthorities().forEach(System.out::println);
+			
+			user.setName(principal.getAttribute("name").toString());
+			fetchedEmail = principal.getAttribute("email");		
+			fetchedEmail = (fetchedEmail == null) ? principal.getAttribute("name").toString().concat("@test.com") : fetchedEmail;
+			user.setEmail(fetchedEmail);
+			user.setProfilePic(principal.getAttribute("picture").toString());
+			user.setEnabled(true);
+			user.setEmailVerified(principal.getAttribute("email_verified"));
+			user.setProvider(Providers.GOOGLE);
+			user.setProviderUserId(principal.getName());
+			user.setRoleList(List.of(ApplicationConstants.ROLE_USER));
+			user.setPassword(new BCryptPasswordEncoder().encode(UUID.randomUUID().toString()));
+			
+		}
+		else if(authorizedClientRegistrationId.equalsIgnoreCase("github")) {
+			
+//			System.out.println("github");
+//			principal.getAttributes().forEach((key, value) -> log.info(key + " => " + value));
+//			log.info(principal.getName());
+//			principal.getAuthorities().forEach(System.out::println);
+			
+			user.setName(principal.getAttribute("login").toString());	
+			fetchedEmail = principal.getAttribute("email");		
+			fetchedEmail = (fetchedEmail == null) ? principal.getAttribute("login").toString().concat("@test.com") : fetchedEmail;	
+			user.setEmail(fetchedEmail);
+			user.setProfilePic(principal.getAttribute("avatar_url").toString());
+			user.setEnabled(true);
+			user.setEmailVerified(false);
+			user.setProvider(Providers.GITHUB);
+			user.setProviderUserId(principal.getName());
+			user.setRoleList(List.of(ApplicationConstants.ROLE_USER));
+			user.setPassword(new BCryptPasswordEncoder().encode(UUID.randomUUID().toString()));
+		}
+		
+	
 		Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
 		if(existingUser.isEmpty()) {
 			userRepository.save(user);
